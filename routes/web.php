c7cc86aff783173b774418c025bcc4451c65c296
jrg_300i <jobran.rodriguez@gmail.com>
2025-11-15 10:37:13 -0400

    se volvio a corregir el archivo routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\LanguageController;
@@ -22,25 +23,16 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/profile/password', [ProfileController::class, 'passwordUpdate'])
         ->name('profile.password.update');
 
-    // Ruta para backup (AGREGAR ESTA LÍNEA)
-        Route::get('/profile/backup', [ProfileController::class, 'backup'])->name('profile.backup');
-
-    Route::post('/profile/backup', [ProfileController::class, 'backup'])->name('profile.backup');
+    // Rutas para backup (CORREGIDAS)
+    Route::get('/profile/backup', [ProfileController::class, 'backup'])->name('profile.backup');
+    Route::post('/profile/backup', [ProfileController::class, 'backupStore'])->name('profile.backup.store');
     Route::get('/profile/stats', [ProfileController::class, 'stats'])->name('profile.stats');
-
 
     // Rutas para Categories
     Route::resource('categories', CategoryController::class);
 
-    // Rutas para Languages
-
-    Route::get('/languages', [LanguageController::class, 'index'])->name('languages.index');
-    Route::get('/languages/create', [LanguageController::class, 'create'])->name('languages.create');
-    Route::post('/languages', [LanguageController::class, 'store'])->name('languages.store');
-    Route::get('/languages/{language}', [LanguageController::class, 'show'])->name('languages.show');
-    Route::get('/languages/{language}/edit', [LanguageController::class, 'edit'])->name('languages.edit');
-    Route::put('/languages/{language}', [LanguageController::class, 'update'])->name('languages.update');
-    Route::delete('/languages/{language}', [LanguageController::class, 'destroy'])->name('languages.destroy');
+    // Rutas para Languages (CONSISTENTES)
+    Route::resource('languages', LanguageController::class);
 
     // Rutas para Snippets
     Route::resource('snippets', SnippetController::class);
