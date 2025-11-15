d14bf19a7ebae8608438049b5f53f6a827ecfad3
jrg_300i <jobran.rodriguez@gmail.com>
2025-11-15 11:09:07 -0400

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
-        //
+        if (env('APP_ENV') === 'production' || env('APP_ENV') === 'staging') {
+            URL::forceScheme('https');
+        }
     }
 }
